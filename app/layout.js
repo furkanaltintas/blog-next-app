32a79ff2872445ea7ca56396fed15a155ef2940d
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-03 16:32:42 +0300

    Layout oluşturuldu.

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import styles from './styles.module.css';
+import '@/styles/global.css';
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="tr">
+            <body className={styles.container}>
+                <header>Header</header>
+                <main>{children}</main>
+                <footer></footer>
+            </body>
+        </html>
+    )
+}
