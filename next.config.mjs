d04ac91f8243a5f77bdb31d62db44f7d7bc47f6a
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-04 01:00:26 +0300

    v1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**' // Tüm pathlere izin verdik
+      }
+    ]
+  },
 };
 
 export default nextConfig;
